13bd3d222cc9f5c21cc7c4763ae18a40f58f7a44
AngadHI <86463735+AngadHI@users.noreply.github.com>
2022-11-01 11:56:28 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,84 @@
+var last_position_of_x, last_position_of_y;
+
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext("2d");
+    
+    color = "blue";
+    width_of_line = 2;
+
+    ctx.beginPath();
+    ctx.strokeStyle="blue";
+    ctx.lineWidth=5;
+    ctx.arc(250,210,40,0,2*Math.PI);
+    ctx.stroke();
+
+    var last_position_of_x, last_position_of_y;
+
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext("2d");
+    
+    color = "yellow";
+    width_of_line = 2;
+
+    ctx.beginPath();
+    ctx.strokeStyle="yellow";
+    ctx.lineWidth=5;
+    ctx.arc(300,250,40,0,2*Math.PI);
+    ctx.stroke();
+
+    var last_position_of_x, last_position_of_y;
+
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext("2d");
+    
+    color = "black";
+    width_of_line = 2;
+
+    ctx.beginPath();
+    ctx.strokeStyle="black";
+    ctx.lineWidth=5;
+    ctx.arc(350,210,40,0,2*Math.PI);
+    ctx.stroke();
+
+    var last_position_of_x, last_position_of_y;
+
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext("2d");
+    
+    color = "red";
+    width_of_line = 2;
+
+    ctx.beginPath();
+    ctx.strokeStyle="red";
+    ctx.lineWidth=5;
+    ctx.arc(450,210,40,0,2*Math.PI);
+    ctx.stroke();
+
+    var last_position_of_x, last_position_of_y;
+
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext("2d");
+    
+    color = "green";
+    width_of_line = 2;
+
+    ctx.beginPath();
+    ctx.strokeStyle="green";
+    ctx.lineWidth=5;
+    ctx.arc(400,250,40,0,2*Math.PI);
+    ctx.stroke();
+
+    var last_position_of_x, last_position_of_y;
+
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext("2d");
+    
+    color = "grey";
+    width_of_line = 2;
+
+    ctx.beginPath();
+    ctx.strokeStyle="grey";
+    ctx.lineWidth=5;
+    ctx.rect(150,143,430,200);
+    ctx.stroke();
+    
